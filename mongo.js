ff417c46302dd77edb63c6a88bc6bab2241a2def
Jaathavan <rjaathu@outlook.com>
2022-08-26 19:02:46 -0400

    fetch data

--- mongo.js
+++ mongo.js
@@ -22,16 +22,23 @@ mongoose
   .then((result) => {
     console.log('connected')
 
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
-      important: true,
+    // const note = new Note({
+    //   content: 'HTML is Easy',
+    //   date: new Date(),
+    //   important: true,
+    // })
+
+    Note.find({}).then(result => {
+        result.forEach(note => {
+          console.log(note)
+        })
+        mongoose.connection.close()
     })
 
-    return note.save()
+    //return note.save()
   })
-  .then(() => {
-    console.log('note saved!')
-    return mongoose.connection.close()
-  })
+//   .then(() => {
+//     console.log('note saved!')
+//     return mongoose.connection.close()
+//   })
   .catch((err) => console.log(err))
