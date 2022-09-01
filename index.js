3ac9feed8f2b90fdfacf623e6316b8a78d026f60
Jaathavan <rjaathu@outlook.com>
2022-08-31 20:17:05 -0400

    validate data via inbuilt mongoose module

--- index.js
+++ index.js
@@ -27,21 +27,17 @@ app.get('/', (request, response) => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (body.content === undefined) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 //send to api/notes
@@ -74,14 +70,13 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context:'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -99,7 +94,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
